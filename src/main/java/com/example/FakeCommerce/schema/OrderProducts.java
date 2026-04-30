1e94bbb16229b5eb3b6d8e7abb1c8ec6bfab08cd
kprashant23 <prashant097kumar@gmail.com>
2026-04-30 12:30:43 +0530

    added create_at and update_at

---
+++ src/main/java/com/example/FakeCommerce/schema/OrderProducts.java
@@ -0,0 +1,30 @@
+package com.example.FakeCommerce.schema;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_products")
+@Builder
+public class OrderProducts extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id", nullable = false)
+  private Order order;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
+
+  private Integer quantity;
+}
